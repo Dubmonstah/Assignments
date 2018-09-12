28a41d5e9a5b40de58624bbd9e434554cba34b88
jonah <jonah_mercera@hotmail.com>
2018-09-12 12:15:31 +0200

    what I tried doing with javascript (Slideshow)

---
+++ Competa Assignments (week3)/js-slider.js
@@ -0,0 +1,22 @@
+
+var slides = new Array();
+slides[0] = document.getElementsByClassName("slide");
+slides[1] = document.getElementsByClassName("slide1");
+slides[2] = document.getElementsByClassName("slide2");
+slides[3] = document.getElementsByClassName("slide3");
+autoplaySlides();
+
+function autoplaySlides(slides){
+    var currentSlide;
+    for (currentSlide = 0; currentSlide < slides.length; currentSlide++){
+        slides[currentSlide].style.display = "none";
+
+    }
+    slides++;
+    if (currentslide =
+
+}
+function navigateSlide(direction){
+
+
+}
